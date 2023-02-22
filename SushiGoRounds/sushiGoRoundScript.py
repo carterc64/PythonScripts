61efa01a89c9988a30939a32603343c343a2beb4
Carterc64 <cwc00001@mix.wvu.edu>
2023-02-22 11:47:48 -0500

    Added sushi roll info and rest of cords

--- SushiGoRounds/sushiGoRoundScript.py
+++ SushiGoRounds/sushiGoRoundScript.py
@@ -2,6 +2,7 @@ import win32api, win32con
 import time
 import os
 
+
 class cords():
     xPad = 364
     yPad = 174
@@ -18,8 +19,56 @@ class cords():
     f_salman = (-36, 411)
     f_unagi = (18, 409)
 
+    plate1 = (20,175)
+    plate2 = (122, 172)
+    plate3 = (228, 174)
+    plate4 = (327, 175)
+    plate5 = (432, 176)
+    plate6 = (531, 176)
+
+    phone = (518, 329)
+
+    m_toppings = (479, 240)
+    m_rice = (477, 261)
+    m_sake = (481, 283)
+
+    t_shrimp = (427, 190)
+    t_unagi = (510, 184)
+    t_nori = (426, 244)
+    t_fishEgg = (514, 242)
+    t_salmon = (428, 300)
+
+    t_rice = (478, 245)
+
+    t_sake = (478, 237)
+
+    deliverN = (425, 256)
+    deliverE = (511, 262)
+
+
+class sushi():
+
+    r_onigiri = [cords.f_rice, cords.f_rice, cords.f_nori]
+    r_caliroll = [cords.f_rice, cords.f_nori, cords.f_roe ]
+    r_gunkan = [cords.f_rice, cords.f_nori, cords.f_roe, cords.f_roe]
+
+    onigiri = (r_onigiri, "onigiri")
+    caliroll = (r_caliroll, "caliroll")
+    gunkan = (r_gunkan, "gunkan")
+
+    sushiList = [onigiri, caliroll, gunkan]
+
+
+
 
 def leftClick():
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+    time.sleep(.1)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+    print ("Click")
+
+def leftClick(cords):
+    mousePos(cords)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
     time.sleep(.1)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
@@ -60,6 +109,38 @@ def startGame():
     mousePos((266, 366))
     leftClick()
     time.sleep(.1)
+
+def clearTables():
+    mousePos(cords.plate1)
+    leftClick()
+    mousePos(cords.plate2)
+    leftClick()
+    mousePos(cords.plate3)
+    leftClick()
+    mousePos(cords.plate4)
+    leftClick()
+    mousePos(cords.plate5)
+    leftClick()
+    mousePos(cords.plate6)
+    leftClick()
+    time.sleep(1)
+
+def foldMat():
+    mousePos(cords.f_rice[0] + 40, cords.f_rice[1])
+    leftClick()
+
+
+def makeFood(food):
+    
+    for item in sushi.sushiList:
+        if (item[1] == food):
+            for item in item[0]:
+                leftClick(item)
+                time.sleep(.1)
+    foldMat()
+    time.sleep(.5)
+    print ("sushi created")
+    time.sleep(1.5)
 
 
 def main():
